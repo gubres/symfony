96117cb3a54edc9b24fa29996c91a1ce7e0578d0
Gustavo Iago <gustavoiago@Gustavos-MacBook-Air.local>
2024-04-14 11:47:29 +0200

    Ajustes de la logica del codigo, y cambios esteticos

---
+++ migrations/Version20240414091017.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240414091017 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE famosos (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(30) NOT NULL, apellido VARCHAR(30) NOT NULL, profesion VARCHAR(30) NOT NULL, eliminado TINYINT(1) NOT NULL, modificado DATETIME NOT NULL, creado DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE famosos');
+    }
+}
